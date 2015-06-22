59c3b0ceb2cb8be74d99f5fca5113ef594a7be0a
Zachary Gordon <z.gordon.h@gmail.com>
2015-06-22 16:05:34 -0700

    final changes

--- public/app.js
+++ public/app.js
@@ -4,7 +4,7 @@ $(function(){
   //Function to add new items and associated links.
   var addItem = function(){
     if($("#userInput").val()==''){
-      alert('Please enter an item.');
+      alert('Please enter an item in the textbox.');
     }else if ($("#userInput").val().toUpperCase()=='GROCERIES'){
       $("#list").append("<a href='http://www.QFC.com/'><li class='check'><input type='checkbox' />" + $("#userInput").val()+ "</li></a>");
       $ ('#userInput').val('');
@@ -17,10 +17,11 @@ $(function(){
     }
   };
 
+  //Function for 'new item' button.
   $("#newItem").click(function(item) {
     addItem();
   });
-  // Function to press enter
+  // Function for enter key.
   $("#userInput").keyup(function(event){
     if(event.keyCode == 13){
       $("#userInput").click();{
@@ -28,15 +29,18 @@ $(function(){
       };
     }
   });
+  //Function for delete button.
   $("#delete").click(function(){
-     $('.check').has('input:checked').remove();
+    $('.check').has('input:checked').remove();
   });
-
+  // Function for update button.
   $('#change').click(function() {
-  $('.check').has('input:checked').html("<li id='check'><input type='checkbox'/>"+ $("#userInput").val()+ "</li>");
-  $('#userInput').val('');
+    if($("#userInput").val()==''){
+      alert('Please enter an updated item in the text box.');
+    }else{
+      $('.check').has('input:checked').html("<li id='check'><input type='checkbox'/>"+ $("#userInput").val()+ "</li>");
+      $('#userInput').val('');
+    }
   });
-
-
 
 });
--- server.js
+++ server.js
@@ -1,18 +1,23 @@
+// Requiring express.
 var express = require('express');
 var app = express();
 
+// Assigning the port.
+// Assigning public as the root folder.
 app.set('port', (process.env.PORT || 5000));
 app.use(express.static('public'));
 
+// Listening for request on port and sends callback.
 app.listen(app.get('port'), function () {
   console.log('Server running');
 });
 
+// Sends root pages like index as a response.
+// Or an error.
 app.use('/', function(req, res) {
   var options = {
     root:__dirname + '/public/',
   };
-
   res.sendFile('./error.html', options, function (error) {
     if (error) {
       console.log(error);
